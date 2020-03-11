57b87e5725cad9db90815e24381d0f326747e025
Кристина <patrakovakris@gmail.com>
2020-03-11 08:02:33 +0400

    feat:add catsCount function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+	
+    if(!Array.isArray(matrix) || matrix.length==0){
+        return 0
+    }
+
+    let cats=0;
+    matrix.map(function (elem) {
+        cats += elem.filter(item => item == "^^").length;
+    })
+    return cats;
 };
