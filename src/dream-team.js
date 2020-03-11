8fa82807802fcbbd667fd8a82dc3705e8a06aaac
Кристина <patrakovakris@gmail.com>
2020-03-11 10:02:15 +0400

    feat:add DreamTeam fonction

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if(!Array.isArray(members)) return false;
+	
+    let code =[];
+    members.map(function(m){
+        if(typeof m =='string')
+            code.push(m.trim().charAt().toUpperCase());
+    })
+
+    return code.sort().join('');
 };
