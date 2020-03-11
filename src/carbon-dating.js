796c4ea455f1123fb68ac0267badfe2de7155872
Кристина <patrakovakris@gmail.com>
2020-03-11 09:26:19 +0400

    feat: add Carbon function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if(typeof sampleActivity !== 'string' ||
+        sampleActivity <= 0 ||
+        /[a-zA-Z]/.test(sampleActivity)||
+        sampleActivity > MODERN_ACTIVITY) return false;
+
+
+        return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };
