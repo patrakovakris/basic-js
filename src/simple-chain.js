5fd8a660f64aa4f755d052ba8260e62b0184de2b
Кристина <patrakovakris@gmail.com>
2020-03-11 16:09:42 +0400

    feat: add chainMaker function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,43 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    chain: [],
+	
+    getLength() {
+        return this.chain.length;
+    },
+
+    addLink(value) {
+        this.chain.push(value);
+        return this;
+    },
+
+	 removeLink(position) {
+		if(isNaN(+position) ||
+		   position == '' ||
+		   this.chain[position] === undefined) {
+			 this.chain = [];
+			 throw new Error();
+		}
+		this.chain.splice(position - 1, 1);
+		return this;
+	  },
+
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+  
+    finishChain() {
+        let result ='';
+
+        this.chain.map((elem,i) =>  {
+            result += (i == 0) ? `( ${elem} )` : `~~( ${elem} )`;
+        })
+
+        this.chain =[];
+        return result;
+
+    }
+
 };
 
 module.exports = chainMaker;
