362a5b0833d39dd94a708f8e35add19997076674
Кристина <patrakovakris@gmail.com>
2020-03-11 10:56:33 +0400

    feat:add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (!options) options = {};
+    if (typeof str !== 'string') str = String(str);
+
+    string='';
+    if(options.repeatTimes === undefined) options.repeatTimes = 1;
+    if(options.separator === undefined) options.separator = '+';
+    if(options.additionSeparator === undefined ) options.additionSeparator='|';
+    if(options.addition === undefined) options.addition = '';
+    if(options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+
+    console.log(options);
+    for(let i=0; i < options.repeatTimes; i++){
+        string += str;
+        for(let j=0; j < options.additionRepeatTimes; j++){
+
+            string += options.addition;
+
+            if(j+1 !== options.additionRepeatTimes){
+                string += options.additionSeparator ;
+            }
+        }
+        if(i+1 !== options.repeatTimes){
+            string += options.separator ;
+        }
+    }
+    // console.log(string);
+    return string;
 };
   
