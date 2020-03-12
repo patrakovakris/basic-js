5b4116b44641616d0c852b4889df7876c2288473
Кристина <patrakovakris@gmail.com>
2020-03-12 08:01:03 +0400

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if(!Array.isArray(arr)) throw new Error();
+    t=[];
+    for (let i = 0; i < arr.length; i++) {
+
+            if (arr[i] === '--discard-next'){
+                // arr.splice(arr[i+1],1);
+                i++;
+            }else if(arr[i] === '--discard-prev'){
+                t.pop();
+            }else if(arr[i] === '--double-next'){
+                t.push(arr[i + 1]);
+            }else if(arr[i] === '--double-prev'){
+                t.push(arr[i - 1]);
+            }else{
+                t.push(arr[i]);
+            }
+
+    }
+
+    // console.log(t);
+    return t.filter(e=>e !==undefined);
 };
